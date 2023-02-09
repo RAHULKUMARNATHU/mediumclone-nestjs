39cdb2a558580ae6eb4c4572adce957025b0ab75
Rahul Kumar <rahul.kumar@brainvire.com>
2023-02-09 17:40:44 +0530

    commit :: basic setup for swagger-ui

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
-if(!process.env.IS_TS_NODE){
-  require('module-alias/register')
+if (!process.env.IS_TS_NODE) {
+  require('module-alias/register');
 }
 
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule , {cors : true});
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+    .setTitle('Medium-clone')
+    .setDescription('The medium API description')
+    .setVersion('1.0')
+    .addTag('mediumclone')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
